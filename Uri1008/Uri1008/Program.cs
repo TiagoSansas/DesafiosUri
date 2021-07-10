b2abd623dde665786c2328111e35b32c4852f47a
Tiago Souza <tiagoeduardosouza@gmail.com>
2021-07-10 09:25:04 -0300

    Resolução Uri1008 - Salario

---
+++ Uri1008/Uri1008/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumFuncionario, horasTrabalhadas;
+            double ValorHora;
+
+            NumFuncionario = int.Parse(Console.ReadLine());
+            horasTrabalhadas = int.Parse(Console.ReadLine());
+            ValorHora = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"NUMBER = {NumFuncionario}");
+            Console.WriteLine($"SALARY = U$ {horasTrabalhadas * ValorHora:F2}",CultureInfo.InvariantCulture);
+        }
+    }
+}
