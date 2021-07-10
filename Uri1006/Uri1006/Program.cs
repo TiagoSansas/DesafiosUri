6d6527bc982aaf091caf15c734714fc3a874f28d
Tiago Souza <tiagoeduardosouza@gmail.com>
2021-07-10 08:46:03 -0300

    Resolução do desafio 1006 - Media Poderada

---
+++ Uri1006/Uri1006/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Nesse exercicio pede novamente a media poderada onde A tem peso 2
+             * B tem peso 3 e C tem peso 5
+             * Onde o peso total e equivalente a 10
+             */
+            double A, B, C, MEDIA;
+
+            A = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            C = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            MEDIA = ((A * 2) + (B * 3) + (C * 5)) / 10;
+            Console.WriteLine($"MEDIA = {MEDIA:F1}");
+
+        }
+    }
+}
