a2813f733210768245a0cfd2f5ac1445b4902aeb
Tiago Souza <tiagoeduardosouza@gmail.com>
2021-07-10 08:37:33 -0300

    Resolução Desafio 1005 - Media Poderada

---
+++ Uri1005/Uri1005/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nesse problema utiliza-se a media podera onde A tem peso 3.5 e B tem peso 7.5
+
+            double A, B, MEDIA;
+            A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            MEDIA = ((A*3.5)+(B*7.5))/11;
+
+            Console.WriteLine($"MEDIA = {MEDIA:F5}");
+
+
+        }
+    }
+}
