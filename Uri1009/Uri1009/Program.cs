cb50f0f8977e8c761d91cb8c93174809e461f698
Tiago Souza <tiagoeduardosouza@gmail.com>
2021-07-11 07:14:40 -0300

    Resolução Desafio Uri1009 - Salario com bonus

---
+++ Uri1009/Uri1009/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salario, totalDeVendas,comissao;
+
+            nome = Console.ReadLine();
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            totalDeVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comissao = (totalDeVendas / 100) * 15 + salario;
+
+            Console.WriteLine($"TOTAL = R$ {comissao:F2}");
+
+        }
+    }
+}
