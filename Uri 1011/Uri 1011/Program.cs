441b18cef4329cdbefef67d1633c2c2f54537918
Tiago Souza <tiagoeduardosouza@gmail.com>
2021-07-12 20:02:38 -0300

    Realização desafio Uri1012 - Área

---
+++ Uri 1011/Uri 1011/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri_1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int raio;
+            double pi = 3.14159,volume;
+
+            raio = int.Parse(Console.ReadLine());
+
+            volume = (4.0 / 3) * ((pi * Math.Pow(raio, 3)));
+
+            Console.WriteLine($"VOLUME = {volume:F3}",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Uri1012/Uri1012/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] valores = Console.ReadLine().Split(' ');
+            double A, B, C;
+            double TrianguloRetangulo, AreaDoCirculo, AreaTrapezio, AreaQuadrado, AreaRetangulo;
+            A = double.Parse(valores[0],CultureInfo.InvariantCulture);
+            B = double.Parse(valores[1],CultureInfo.InvariantCulture);
+            C = double.Parse(valores[2],CultureInfo.InvariantCulture);
+
+            TrianguloRetangulo = A * C / 2;
+            AreaDoCirculo = Math.Pow(C, 2) * 3.14159;
+            AreaTrapezio = (A + B) * C / 2;
+            AreaQuadrado = Math.Pow(B, 2);
+            AreaRetangulo = A * B;
+
+            Console.WriteLine($"TRIANGULO: {TrianguloRetangulo:F3}");
+            Console.WriteLine($"CIRCULO: {AreaDoCirculo:F3}");
+            Console.WriteLine($"TRAPEZIO: {AreaTrapezio:F3}");
+            Console.WriteLine($"QUADRADO: {AreaQuadrado:F3}");
+            Console.WriteLine($"RETANGULO: {AreaRetangulo:F3}");
+            
+            
+            
+
+
+        }
+    }
+}
