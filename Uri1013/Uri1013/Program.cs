c12e7838319d1bd7886dab2e77ca6edaa95536c8
Tiago Souza <tiagoeduardosouza@gmail.com>
2021-07-12 20:23:21 -0300

    Resolucao desafio Uri1013 - O Maior

---
+++ Uri1013/Uri1013/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uri1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int A, B, C , maior, maiorABC;
+
+            A = int.Parse(valores[0]);
+            B = int.Parse(valores[1]);
+            C = int.Parse(valores[2]);
+
+            maior = (A + B + Math.Abs(A - B)) / 2;
+            maiorABC = (maior + C + Math.Abs(maior - C)) / 2;
+            Console.WriteLine($"{maiorABC} eh o maior");
+
+        }
+    }
+}
