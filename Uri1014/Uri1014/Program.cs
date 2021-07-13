75d01500d7df7b1f2153f8e3aa51e3b71908f31e
Tiago Souza <tiagoeduardosouza@gmail.com>
2021-07-13 20:15:17 -0300

    Resolucao Desafio Uri1014

---
+++ Uri1014/Uri1014/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Uri1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X;
+            double Y, distancia;
+            X = int.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            distancia = X / Y;
+
+            Console.WriteLine($"{distancia:F3} km/l");
+
+
+        }
+    }
+}
